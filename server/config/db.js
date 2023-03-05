6e62b4cdc554d2a5f9bb1a1df7dfafe9f7495ddc
khanjancentennial <khanjancentennial@gmail.com>
2023-03-04 19:03:05 -0500

    First Commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://khanjancentennial:851m5bihDhuMpZrw@cluster1.nr8iigw.mongodb.net/midtearm_exam?retryWrites=true&w=majority"
+};
